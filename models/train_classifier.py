695c915bed6d0b42e1f0ab1726add24df48ebf0f
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2020-05-02 09:27:21 -0600

    Add classifier random forest code

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,10 +1,7 @@
+# import libraries
 import numpy as np 
 import pandas as pd 
 import re
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
@@ -12,17 +9,62 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 import sqlite3
+import nltk
+import datetime
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 # read data from SQLlite 
 conn = sqlite3.connect('../data/disaster_tweets.db')
 df = pd.read_sql('SELECT * FROM messages', conn)
 X = df['message']
-Y = df[df.columns[3:]]
+Y = df[df.columns[4:]]
 
 lemmatizer = WordNetLemmatizer()
-def _tokenize(text):
+def tokenize(text):
     inner_text = re.sub(r"[^a-zA-Z0-1]"," ", text.lower())
     tokens = word_tokenize(inner_text)
-    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stopwords.words('english')]
+    stop_words = stopwords.words('english')
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
     return tokens
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+pipeline = Pipeline([
+    ('vect',CountVectorizer(tokenizer=tokenize, ngram_range=(1,2))),
+    ('tfidf', TfidfTransformer()),
+    ('clf', RandomForestClassifier())
+])
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y)
+
+from sklearn.model_selection import GridSearchCV
+parameters = {
+        'vect__ngram_range': ((1,1),(1, 2)),
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'vect__max_features': (None, 1000, 2000),
+        'tfidf__use_idf': (True, False),
+        'clf__n_estimators': [20, 100, 200],
+        'clf__min_samples_split': [2, 3, 4]
+    }
+
+cv = GridSearchCV(pipeline, param_grid=parameters)
+
+print('Training the model...')
+s_t = datetime.datetime.now()
+pipeline.fit(X_train, y_train)
+y_pred = pipeline.predict(X_test)
+print('accuracy_score',accuracy_score(y_test, y_pred))
+e_t = datetime.datetime.now()
+print(e_t - s_t)
+
+import pickle
+# save the model to disk
+pickle.dump(pipeline, open('model.pkl', 'wb'))
