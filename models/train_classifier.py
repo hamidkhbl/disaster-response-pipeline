b4761287a64842a47a9f0cf2751d2c425e2f2937
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2020-05-05 08:17:27 -0600

    Refactore code - Accuracy: 0.2644

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -6,21 +6,28 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
+
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.multioutput import MultiOutputClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.model_selection import GridSearchCV
-from sklearn import svm
+
+import sys
 import pickle
 import sqlite3
+
 import nltk
-import datetime
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
@@ -41,54 +48,60 @@ def tokenize(text):
     tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
     return tokens
 
-def train_clf(df):
+def train_model(df):
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(), n_jobs=1)),
+    ])
+
+    parameters = {  
+    #'vect__ngram_range': ((1,1),(1, 2)),
+    'tfidf__use_idf': (True, False)
+    }
+    
+    model = GridSearchCV(pipeline, param_grid=parameters, cv=2, verbose=1)
 
     X = df['message']
     Y = df[df.columns[3:]]
-    pipeline = Pipeline([
-        ('vect',CountVectorizer(tokenizer=tokenize)),
-        ('tfidf', TfidfTransformer()),
-        ('clf', RandomForestClassifier(max_depth=3))
-    ])
-
-    from sklearn.model_selection import train_test_split
+    
     X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state = 42)
 
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
 
-    # parameters = {
-    #         'vect__ngram_range': ((1,1),(1, 2)),
-    #         'vect__max_df': (0.5, 0.75, 1.0),
-    #         'vect__max_features': (None, 1000, 2000),
-    #         'tfidf__use_idf': (True, False),
-    #         'clf__n_estimators': [20, 100, 200],
-    #         'clf__min_samples_split': [2, 3, 4]
-    #     }
+    print("Classification:")
+    print(classification_report(y_test, y_pred)) 
 
-    # cv = GridSearchCV(pipeline, param_grid=parameters)
+    print("Accuracy Score:")
+    print(accuracy_score(y_test, y_pred)) 
+
+    return model
+
+def save_model(model):
+    # save the model to disk
+    pickle.dump(model, open('model.pkl', 'wb'))
+
+
+      
+def main():
+    try:
+        db_name = argv[1]
+    except:
+        try:
+            db_name = r"..\data\disaster_tweets.db"
+        except:
+            print('Database not found')
 
     print('Training the model...')
-    s_t = datetime.datetime.now()
+    model = train_model(read_data(db_name))
 
-    pipeline.fit(X_train, y_train)
-    y_pred = pipeline.predict(X_test)
-    print('accuracy_score:',accuracy_score(y_test, y_pred))
+    print('Saving the model...')
+    save_model(model)
 
-    e_t = datetime.datetime.now()
-    print(e_t - s_t)
-
-    # save the model to disk
-    pickle.dump(pipeline, open('model.pkl', 'wb'))
-
-try:
-    import sys
-    db_name = sys.argv[1]
-except:
-    db_name = '..\data\disaster_tweets.db'
-    
-def main():
-    train_clf(read_data(db_name))
-
-main()
+if __name__ == "__main__":
+    main()
 
 
 
